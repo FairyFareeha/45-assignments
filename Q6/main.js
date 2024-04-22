f9d35e64ab98526e579bacf4d5e47fd31a06f8e9
FairyFareeha <160640251+FairyFareeha@users.noreply.github.com>
2024-04-22 19:32:11 +0500

    Q6

---
+++ Q6/main.js
@@ -0,0 +1,5 @@
+"use strict";
+const personName = "   \n\tMhammad Mustafa\t\n    ";
+console.log(personName);
+const personName2 = personName.trim();
+console.log(personName2);
---
+++ Q6/main.ts
@@ -0,0 +1,5 @@
+const personName : string = "   \n\tMhammad Mustafa\t\n    ";
+console.log(personName);
+const personName2 : string = personName.trim();
+console.log(personName2);
+
