f330c96952fe2462a4f467edbdf20e944088b132
FairyFareeha <160640251+FairyFareeha@users.noreply.github.com>
2024-04-23 21:33:55 +0500

    Q9

---
+++ Q9/q9.js
@@ -0,0 +1,5 @@
+//Favourite number : Store your favourite number in a variable. Then,using that variable,create a message that reveals your favourite number.Print that Message.
+const favourit_Number = 5;
+const message = `This Is My Favourite Number : ${favourit_Number}`;
+console.log(message);
+export {};
---
+++ Q9/q9.ts
@@ -0,0 +1,4 @@
+//Favourite number : Store your favourite number in a variable. Then,using that variable,create a message that reveals your favourite number.Print that Message.
+const favourit_Number : number = 5;
+const message : string = `This Is My Favourite Number : ${favourit_Number}`
+console.log(message);
