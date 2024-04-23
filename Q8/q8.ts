67ee6a4e16c265df1f8baed4682cda01a241f707
FairyFareeha <160640251+FairyFareeha@users.noreply.github.com>
2024-04-23 21:06:24 +0500

    Q8 commit

---
+++ Q8/q8.ts
@@ -0,0 +1,11 @@
+// You should create four lines that look like this : 
+// console.log(5+3) 
+// Your output should simply be four lines with the number 8 appearing on each other. 
+console.log(5+3);
+console.log(5+3);
+console.log(5+3);
+console.log(5+3);
+
+
+
+
