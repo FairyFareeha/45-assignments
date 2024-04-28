5dfbeb5eb5e86d269e56df6bd7a246b3dacdd494
FairyFareeha <160640251+FairyFareeha@users.noreply.github.com>
2024-04-28 20:49:35 +0500

    Q10

---
+++ Q10/part2.js
@@ -0,0 +1,5 @@
+// Name : Fareeha Khurram
+// Date : 24-4-24
+//This program will add two digits.
+console.log(2 + 2);
+export {};
---
+++ Q10/q10.ts
@@ -0,0 +1,8 @@
+//Adding commnts : Choose two of the programs you've written,and add atleast one comment to each.If you don't have anything specific to write because your programs are too simple at this point,just add your name and the current date at the top of each program file.Then write one sentence describing what the program does.
+
+// Name : Fareeha Khurram
+// Date : 24-4-24
+
+//log Hello World
+
+console.log("Hello World");
