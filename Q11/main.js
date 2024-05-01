a366f86df61677b5ba7ad3c4feee560758da82a5
FairyFareeha <160640251+FairyFareeha@users.noreply.github.com>
2024-05-01 18:18:19 +0500

    Q11

---
+++ Q11/main.js
@@ -0,0 +1,6 @@
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+let friends_names = ['Roma', 'Saima', 'Afshan'];
+for (let i = 0; i < friends_names.length; i++) {
+    console.log(friends_names[i]);
+}
+export {};
---
+++ Q11/main.ts
@@ -0,0 +1,8 @@
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+let friends_names : string[] = ['Roma','Saima','Afshan']
+
+for (let i = 0; i<friends_names.length; i++) {
+    console.log(friends_names[i]);
+    
+}
