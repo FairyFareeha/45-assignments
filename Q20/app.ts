b12e1c7afe905c2c1a40b7aaa5b35de3a89d985e
FairyFareeha <160640251+FairyFareeha@users.noreply.github.com>
2024-05-10 23:18:07 +0500

    Q20

---
+++ Q20/app.ts
@@ -0,0 +1,9 @@
+//Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+import { toASCII } from "punycode";
+
+let array : string[] = ['Urdu','English','French'];
+console.log( "I would like to learn : " + array);
+
+
+ 
