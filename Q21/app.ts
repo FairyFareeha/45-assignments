6fbd0a89d30d9b143d7dc5e5865f36550f321907
FairyFareeha <160640251+FairyFareeha@users.noreply.github.com>
2024-05-12 15:36:32 +0500

    Q21

---
+++ Q21/app.ts
@@ -0,0 +1,18 @@
+//They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+ interface person  {
+    name : string,
+    age : number,
+    isStudent : boolean
+};
+
+let person = {
+name : "Muhammad Mustafs",
+age : 11,
+isStudent : true
+}
+console.log(person);
+
+
+
+
