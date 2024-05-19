15a9fd1506a2815938c8a831ccf38ca39a2219d9
FairyFareeha <160640251+FairyFareeha@users.noreply.github.com>
2024-05-19 23:47:29 +0500

    Q31

---
+++ Q31/main.js
@@ -0,0 +1,12 @@
+// No Users: Add an if test to Exercise 30 to make sure the list of users is not empty.
+// • If the list is empty, print the message We need to find some users!
+// • Remove all of the usernames from your array, and make sure the correct message is printed.
+let myArray = ['Admin', 'Taha', 'Kamran', 'Saleem', 'Daniyal'];
+if (myArray.length === 0) {
+    console.log('We need to find some users!');
+}
+else {
+    myArray = [];
+    console.log('All user names have been removed ' + myArray.length);
+}
+export {};
